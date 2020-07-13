322b842f7c54e86cd891d72c8425b7d176fedea5
M4RC0 4NT0N10 <marco@spaziocurvo.com>
2020-07-13 15:07:25 +0200

    switch back to first port name

--- common/BPlayer.h
+++ common/BPlayer.h
@@ -2,9 +2,5 @@
 
 namespace BPlayer {
     // local socket
-    constexpr const char*   streamPortName =    "io.bplayer.stream.0";
-
-    //  or tcpip localhost:number socket
-    constexpr const char*   locolhost =         "localhost";
-    constexpr   int         streamPortNumber =  61000;
+    constexpr const char*   streamPortName =    "io.bplayer.data.0";
 }
